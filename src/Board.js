15b0ef4ea2adb45924ab73914fe76ccf6d8fcac4
KamilMachowski <45885548+KamilMachowski@users.noreply.github.com>
2019-12-11 17:09:03 +0100

    working display of 20x20 buttons matrix

--- src/Board.js
+++ src/Board.js
@@ -2,11 +2,7 @@ import React from "react";
 //import "./Board.css";
 
 function Block(props) {
-  return (
-    <button className="block">
-      {props.value}
-    </button>
-  );
+  return <button className="block">{props.value}</button>;
 }
 
 class Board extends React.Component {
@@ -14,23 +10,27 @@ class Board extends React.Component {
     return (
       <Block
         value={this.props.blocks[k]}
-       // onClick={() => this.props.onClick(k)}
+        // onClick={() => this.props.onClick(k)}
       />
     );
   }
   renderRow(i) {
+    var row = [];
     for (var j = 0; j < 20; j++) {
-      return this.renderBlock(i * 20 + j);
+      row.push(this.renderBlock(i * 20 + j));
     }
+    return row;
   }
   renderBoard() {
+    var board = [];
     for (var i = 0; i < 20; i++) {
-      return (
+      board.push(
         <div className="board-row">
           <div>{this.renderRow(i)}</div>
         </div>
       );
     }
+    return board;
   }
 
   render() {
